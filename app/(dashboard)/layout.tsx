883b74b4264493c8cd83734da571b1ac9bcd1702
IvelisBecker <ivelisbecker@gmail.com>
2025-11-15 13:48:59 -0600

    Added SideNav from nextjs dashboard tutorial

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,18 @@
+import SideNav from '../ui/dashboard/sidenav';
 import Footer from '@/app/ui/footer';
 
-export default function DashboardLayout({ 
-      children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <>
-      <main>{children}</main>
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+    <div className="flex min-h-screen flex-col">
+      <div className="flex flex-1 flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+          <SideNav />
+        </div>
+        <div className="flex-1 p-6 md:overflow-y-auto md:p-12">
+          <main>{children}</main>
+        </div>
+      </div>
       <Footer />
-    </>
-  )
+    </div>
+    )
 }
